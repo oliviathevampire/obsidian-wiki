fec1be5b15629dc02968db510b0eeb8be43bee4e
OliviaTheVampire <sindrefagerheim53@gmail.com>
2023-04-30 15:12:11 +0200

    updating stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,27 @@
+import { createResolver } from '@nuxt/kit'
+const { resolve } = createResolver(import.meta.url)
+
+
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus'
-})
+  extends: '@nuxt-themes/docus',
+  nitro: {
+    preset: 'service-worker'
+  },
+  target: 'static',
+  router: {
+    base: '/obsidian-wiki/'
+  },
+  content: {
+    documentDriven: true,
+    highlight: {
+      theme: {
+        dark: 'github-dark',
+        default: 'github-light'
+      },
+      preload: ['json', 'js', 'ts', 'html', 'css', 'vue', 'diff', 'shell', 'markdown', 'yaml', 'bash', 'ini']
+    },
+    navigation: {
+      fields: ['icon', 'titleTemplate', 'header', 'main', 'aside', 'footer']
+    }
+  },
+});
